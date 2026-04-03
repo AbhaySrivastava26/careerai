5e413a4280e54298b9143d43aa5f0a89d0da4406
AbhaySrivastava26 <abhaysrivastava2627@gmail.com>
2026-04-03 21:44:41 +0530

    clean commit without secrets

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ app = FastAPI(
     version="5.0.0",
 )
 
-CORS_ORIGINS = os.getenv("CORS_ORIGINS", "http://localhost:5173").split(",")
+CORS_ORIGINS = os.getenv("CORS_ORIGINS", "*").split(",")
 
 app.add_middleware(
     CORSMiddleware,
